a6ff0937ff114900bf01a78fd13241e137bedb63
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-05 22:13:15 +0100

    Add trial script at scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var withButtonItemsClass = document.getElementsByClassName('button');
+for (var i = 0; i < withButtonItemsClass.length; i++) {
+  alert(document.getElementById('button').innerHTML);
+}
